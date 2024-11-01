492d8803e1d442b7f16780adf2ef20de6bdaba7c
akitenkrad <akitenkrad@gmail.com>
2024-11-01 10:24:52 +0900

    dev in progress

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -13,6 +13,13 @@ use std::process::Stdio;
 mod tests;
 
 #[derive(Debug, Clone, PartialEq)]
+pub enum BlockAttr {
+    Title,
+    Text,
+    Else,
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct Word {
     pub text: String,
     pub x: f32,
@@ -48,7 +55,7 @@ impl Line {
     }
     pub fn add_word(&mut self, text: String, x: f32, y: f32, width: f32, height: f32) {
         self.words.push(Word {
-            text: text,
+            text: text.trim().to_string(),
             x: x,
             y: y,
             width: width,
@@ -56,12 +63,11 @@ impl Line {
         });
     }
     pub fn get_text(&self) -> String {
-        let mut text = String::new();
+        let mut words = Vec::new();
         for word in &self.words {
-            text.push_str(&word.text);
-            text.push_str(" ");
+            words.push(word.text.clone());
         }
-        return text;
+        return words.join(" ");
     }
 }
 
@@ -72,6 +78,7 @@ pub struct Block {
     pub y: f32,
     pub width: f32,
     pub height: f32,
+    pub attr: BlockAttr,
 }
 
 impl Block {
@@ -82,6 +89,7 @@ impl Block {
             y: y,
             width: width,
             height: height,
+            attr: BlockAttr::Else,
         }
     }
     pub fn add_line(&mut self, x: f32, y: f32, width: f32, height: f32) {
@@ -112,9 +120,11 @@ impl Page {
             height: height,
         }
     }
+
     pub fn add_block(&mut self, x: f32, y: f32, width: f32, height: f32) {
         self.blocks.push(Block::new(x, y, width, height));
     }
+
     pub fn get_text(&self) -> String {
         let mut text = String::new();
         for block in &self.blocks {
@@ -123,8 +133,198 @@ impl Page {
         }
         return text;
     }
+
+    pub fn top(&self) -> f32 {
+        let mut values: Vec<f32> = Vec::new();
+        for block in &self.blocks {
+            for line in &block.lines {
+                values.push(line.y);
+            }
+        }
+        values.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        return values.first().unwrap().clone();
+    }
+
+    pub fn bottom(&self) -> f32 {
+        let mut values: Vec<f32> = Vec::new();
+        for block in &self.blocks {
+            for line in &block.lines {
+                values.push(line.y + line.height);
+            }
+        }
+        values.sort_by(|a, b| b.partial_cmp(a).unwrap());
+        return values.first().unwrap().clone();
+    }
+    pub fn left(&self) -> f32 {
+        let mut values: Vec<f32> = Vec::new();
+        for block in &self.blocks {
+            for line in &block.lines {
+                values.push(line.x);
+            }
+        }
+        values.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        return values.first().unwrap().clone();
+    }
+
+    pub fn right(&self) -> f32 {
+        let mut values: Vec<f32> = Vec::new();
+        for block in &self.blocks {
+            for line in &block.lines {
+                values.push(line.x + line.width);
+            }
+        }
+        values.sort_by(|a, b| b.partial_cmp(a).unwrap());
+        return values.first().unwrap().clone();
+    }
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub struct Point {
+    pub x: f32,
+    pub y: f32,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Coordinate {
+    pub top_left: Point,
+    pub top_right: Point,
+    pub bottom_left: Point,
+    pub bottom_right: Point,
+}
+
+impl Coordinate {
+    pub fn from_rect(x1: f32, y1: f32, x2: f32, y2: f32) -> Coordinate {
+        Coordinate {
+            top_left: Point { x: x1, y: y1 },
+            top_right: Point { x: x2, y: y1 },
+            bottom_left: Point { x: x1, y: y2 },
+            bottom_right: Point { x: x2, y: y2 },
+        }
+    }
+
+    pub fn from_object(x: f32, y: f32, width: f32, height: f32) -> Coordinate {
+        Coordinate {
+            top_left: Point { x: x, y: y },
+            top_right: Point { x: x + width, y: y },
+            bottom_left: Point { x: x, y: y + height },
+            bottom_right: Point {
+                x: x + width,
+                y: y + height,
+            },
+        }
+    }
+
+    pub fn width(&self) -> f32 {
+        return self.top_right.x - self.top_left.x;
+    }
+
+    pub fn height(&self) -> f32 {
+        return self.bottom_left.y - self.top_left.y;
+    }
+
+    pub fn is_intercept(&self, other: &Coordinate) -> bool {
+        if self.top_left.x >= other.bottom_right.x || self.bottom_right.x <= other.top_left.x {
+            return false;
+        }
+        if self.top_left.y >= other.bottom_right.y || self.bottom_right.y <= other.top_left.y {
+            return false;
+        }
+        return true;
+    }
+
+    pub fn get_area(&self) -> f32 {
+        return self.width() * self.height();
+    }
+
+    pub fn intersection(&self, other: &Coordinate) -> Coordinate {
+        let x1 = f32::max(self.top_left.x, other.top_left.x);
+        let y1 = f32::max(self.top_left.y, other.top_left.y);
+        let x2 = f32::min(self.bottom_right.x, other.bottom_right.x);
+        let y2 = f32::min(self.bottom_right.y, other.bottom_right.y);
+        return Coordinate::from_rect(x1, y1, x2, y2);
+    }
+
+    pub fn iou(&self, other: &Coordinate) -> f32 {
+        let dx = f32::min(self.bottom_right.x, other.bottom_right.x) - f32::max(self.top_left.x, other.top_left.x);
+        let dy = f32::min(self.bottom_right.y, other.bottom_right.y) - f32::max(self.top_left.y, other.top_left.y);
+
+        if dx <= 0.0 || dy <= 0.0 {
+            return 0.0;
+        } else {
+            let area1 = self.width() * self.height();
+            let area2 = other.width() * other.height();
+            let inter_area = dx * dy;
+            return inter_area / (area1 + area2 - inter_area);
+        }
+    }
+}
+
+pub fn get_font_sizes(pages: &Vec<Page>) -> f32 {
+    let mut font_sizes: Vec<f32> = Vec::new();
+    let text_area = get_text_area(&pages);
+    for page in pages {
+        for block in &page.blocks {
+            for line in &block.lines {
+                let line_coord = Coordinate::from_object(line.x, line.y, line.width, line.height);
+
+                let iou = text_area.iou(&line_coord);
+                let intersection = text_area.intersection(&line_coord).get_area();
+                let line_area = line_coord.get_area();
+
+                if iou > 0.0 && intersection / line_area > 0.9 {
+                    font_sizes.push(line.height);
+                }
+            }
+        }
+    }
+    let normal_font = sci_rs::stats::median(font_sizes.iter()).0;
+    return normal_font;
+}
+
+pub fn get_text_area(pages: &Vec<Page>) -> Coordinate {
+    let mut left_values: Vec<f32> = Vec::new();
+    let mut right_values: Vec<f32> = Vec::new();
+    let mut top_values: Vec<f32> = Vec::new();
+    let mut bottom_values: Vec<f32> = Vec::new();
+
+    for page in pages {
+        left_values.push(page.left());
+        right_values.push(page.right());
+        top_values.push(page.top());
+        bottom_values.push(page.bottom());
+    }
+
+    let left = sci_rs::stats::median(left_values.iter()).0;
+    let right = sci_rs::stats::median(right_values.iter()).0;
+    let top = sci_rs::stats::median(top_values.iter()).0;
+    let bottom = sci_rs::stats::median(bottom_values.iter()).0;
+
+    return Coordinate {
+        top_left: Point { x: left, y: top },
+        top_right: Point { x: right, y: top },
+        bottom_left: Point { x: left, y: bottom },
+        bottom_right: Point { x: right, y: bottom },
+    };
+}
+
+pub fn get_block_attr(block: &Block, font_size: f32, text_area: &Coordinate) -> BlockAttr {
+    let num_lines = block.lines.len();
+    let block_font_size = block.lines.iter().map(|line| line.height).sum::<f32>() / num_lines as f32;
+    let block_coord = Coordinate::from_object(block.x, block.y, block.width, block.height);
+
+    let iou = text_area.iou(&block_coord);
+    let intersection = text_area.intersection(&block_coord).get_area();
+    let block_area = block_coord.get_area();
+    let is_in_text_area = iou > 0.0 && intersection / block_area > 0.9;
+
+    if is_in_text_area && num_lines == 1 && block_font_size > font_size {
+        return BlockAttr::Title;
+    } else if is_in_text_area && num_lines > 1 {
+        return BlockAttr::Text;
+    } else {
+        return BlockAttr::Else;
+    }
+}
 async fn save_pdf(path_or_url: &str) -> Result<String> {
     let mut rng = rand::thread_rng();
     let random_value = rng.gen_range(10000..99999);
@@ -185,8 +385,7 @@ pub async fn pdf2html(path: &str) -> Result<html::Html> {
 
     let mut html = String::new();
     let mut f = File::open(html_path.clone()).expect("file not found");
-    f.read_to_string(&mut html)
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut html).expect("something went wrong reading the file");
     let html = scraper::Html::parse_document(&html);
 
     if Path::new(save_path.as_str()).exists() {
@@ -215,12 +414,7 @@ pub fn parse_html(html: &Html) -> Result<Vec<Page>> {
             let block_ymin = block.value().attr("ymin").unwrap().parse::<f32>().unwrap();
             let block_xmax = block.value().attr("xmax").unwrap().parse::<f32>().unwrap();
             let block_ymax = block.value().attr("ymax").unwrap().parse::<f32>().unwrap();
-            let mut _block = Block::new(
-                block_xmin,
-                block_ymin,
-                block_xmax - block_xmin,
-                block_ymax - block_ymin,
-            );
+            let mut _block = Block::new(block_xmin, block_ymin, block_xmax - block_xmin, block_ymax - block_ymin);
 
             let line_selector = scraper::Selector::parse("line").unwrap();
             let _lines = block.select(&line_selector);
@@ -229,12 +423,7 @@ pub fn parse_html(html: &Html) -> Result<Vec<Page>> {
                 let line_ymin = line.value().attr("ymin").unwrap().parse::<f32>().unwrap();
                 let line_xmax = line.value().attr("xmax").unwrap().parse::<f32>().unwrap();
                 let line_ymax = line.value().attr("ymax").unwrap().parse::<f32>().unwrap();
-                let mut _line = Line::new(
-                    line_xmin,
-                    line_ymin,
-                    line_xmax - line_xmin,
-                    line_ymax - line_ymin,
-                );
+                let mut _line = Line::new(line_xmin, line_ymin, line_xmax - line_xmin, line_ymax - line_ymin);
 
                 let word_selector = scraper::Selector::parse("word").unwrap();
                 let _words = line.select(&word_selector);
@@ -244,13 +433,7 @@ pub fn parse_html(html: &Html) -> Result<Vec<Page>> {
                     let word_xmax = word.value().attr("xmax").unwrap().parse::<f32>().unwrap();
                     let word_ymax = word.value().attr("ymax").unwrap().parse::<f32>().unwrap();
                     let text = word.text().collect::<String>();
-                    _line.add_word(
-                        text,
-                        word_xmin,
-                        word_ymin,
-                        word_xmax - word_xmin,
-                        word_ymax - word_ymin,
-                    );
+                    _line.add_word(text, word_xmin, word_ymin, word_xmax - word_xmin, word_ymax - word_ymin);
                 }
                 _block.lines.push(_line);
             }
@@ -258,5 +441,14 @@ pub fn parse_html(html: &Html) -> Result<Vec<Page>> {
         }
         pages.push(_page);
     }
+
+    let font_size = get_font_sizes(&pages);
+    let text_area = get_text_area(&pages);
+    for page in &mut pages {
+        for block in &mut page.blocks {
+            block.attr = get_block_attr(block, font_size, &text_area);
+        }
+    }
+
     return Ok(pages);
 }
--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -50,4 +50,41 @@ async fn test_parse_html() {
         text.trim(),
         "Provided proper attribution is provided, Google hereby grants permission to"
     );
+
+    for page in pages {
+        for block in page.blocks {
+            let text = block.get_text();
+            let attr = block.attr;
+            println!("[{:?}]: {}", attr, text.trim());
+        }
+    }
+}
+
+#[test]
+fn test_coordinate_is_intercept() {
+    let a = Coordinate::from_rect(0.0, 0.0, 10.0, 10.0);
+    let b = Coordinate::from_rect(5.0, 5.0, 15.0, 15.0);
+    let c = Coordinate::from_rect(15.0, 15.0, 25.0, 25.0);
+    let d = Coordinate::from_rect(0.0, 0.0, 5.0, 5.0);
+    let e = Coordinate::from_rect(20.0, 5.0, 25.0, 10.0);
+    let f = Coordinate::from_rect(5.0, 20.0, 10.0, 25.0);
+
+    assert!(a.is_intercept(&b));
+    assert!(!a.is_intercept(&c));
+    assert!(a.is_intercept(&d));
+    assert!(!a.is_intercept(&e));
+    assert!(!a.is_intercept(&f));
+    assert!(!b.is_intercept(&c));
+    assert!(!b.is_intercept(&d));
+    assert!(!b.is_intercept(&e));
+    assert!(!b.is_intercept(&f));
+}
+
+#[tokio::test]
+async fn test_get_font_sizes() {
+    let url = "https://arxiv.org/pdf/1706.03762";
+    let res = pdf2html(url).await.unwrap();
+    let pages = parse_html(&res).unwrap();
+    let font_sizes = get_font_sizes(&pages);
+    assert!(font_sizes > 0.0);
 }
